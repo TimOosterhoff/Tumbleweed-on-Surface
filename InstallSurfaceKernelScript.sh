99fe0637c27a070d7b21f268fe1266272f7248ca
Tim Oosterhoff <taoosterhoff@gmail.com>
2026-02-04 13:40:18 +0100

    Update InstallSurfaceKernelScript.sh

--- InstallSurfaceKernelScript.sh
+++ InstallSurfaceKernelScript.sh
@@ -8,7 +8,7 @@ clear -x
 echo "Running script: $0"; echo
 
 # Running on supported os $NAME?
-SupportedOsNames=' openSUSE Tumbleweed AnotherSpaceAroundedOsName '
+SupportedOsNames='openSUSE Tumbleweed AnotherSpaceDelimitedOsName'
 source /etc/os-release && [[ ! " $SupportedOsNames " =~ " $NAME " ]] && { echo "Script not designed/tested on $NAME"; exit 99; }
 
 # root privileges?
